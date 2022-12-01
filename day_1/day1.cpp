205b57a5eda5fff67fe0422a970637deb810c199
James Cumming <5695802+jbcumming@users.noreply.github.com>
2022-12-01 22:19:18 +0900

    feat: add day1 part 2

--- day_1/day1.cpp
+++ day_1/day1.cpp
@@ -1,28 +1,32 @@
 // Copyright 2022 jbcumming
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <vector>
 
 int main() {
   std::string line;
-  int highestCalories = 0;
   int currentSum = 0;
+  std::vector<int> calorieTotals;
 
   std::ifstream calorieInputFile("input.txt");
-
   while (std::getline(calorieInputFile, line)) {
     if (line == "") {
-      if (currentSum > highestCalories) {
-        highestCalories = currentSum;
-      }
+      calorieTotals.push_back(currentSum);
       currentSum = 0;
     } else {
       currentSum += std::stoi(line);
     }
   }
-
   calorieInputFile.close();
 
-  std::cout << highestCalories << "\n";
+  std::sort(calorieTotals.begin(), calorieTotals.end(), std::greater<int>());
+  size_t topX = 3;
+  int total = 0;
+
+  for (size_t i = 0; i < topX; ++i) {
+    total += calorieTotals.at(i);
+  }
+  std::cout << total << "\n";
 }
