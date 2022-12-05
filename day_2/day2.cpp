3e70c5ab028f50b9a915e2ddcc8e7af16d72dab0
James Cumming <5695802+jbcumming@users.noreply.github.com>
2022-12-06 00:52:20 +0900

    feat: add day2 part 2

--- day_2/day2.cpp
+++ day_2/day2.cpp
@@ -10,6 +10,7 @@
 #define LOSE 0
 
 enum shape { rock, paper, scissors };
+enum result { win, draw, lose };
 
 shape getShape(char code) {
   shape codeShape;
@@ -30,6 +31,38 @@ shape getShape(char code) {
   return codeShape;
 }
 
+result getResult(char code) {
+  result res;
+  switch (code) {
+  case 'X':
+    res = lose;
+    break;
+  case 'Y':
+    res = draw;
+    break;
+  case 'Z':
+    res = win;
+    break;
+  }
+  return res;
+}
+
+int resultScore(result res) {
+  int score = 0;
+  switch (res) {
+  case win:
+    score = WIN;
+    break;
+  case draw:
+    score = DRAW;
+    break;
+  case lose:
+    score = LOSE;
+    break;
+  }
+  return score;
+}
+
 int shapeScore(shape shape) {
   int score = 0;
   switch (shape) {
@@ -46,7 +79,7 @@ int shapeScore(shape shape) {
   return score;
 }
 
-int resultScore(shape opponent, shape player) {
+int matchScore(shape opponent, shape player) {
   if (opponent == player) {
     return DRAW;
   }
@@ -71,18 +104,44 @@ int resultScore(shape opponent, shape player) {
   return score;
 }
 
+shape getPlay(shape opponent, result res) {
+  if (res == draw) {
+    return opponent;
+  }
+  shape play;
+  switch (opponent) {
+  case rock:
+    play = (res == win) ? paper : scissors;
+    break;
+  case paper:
+    play = (res == win) ? scissors : rock;
+    break;
+  case scissors:
+    play = (res == win) ? rock : paper;
+    break;
+  }
+  return play;
+}
+
 int main() {
   std::string line;
   int totalScore = 0;
+  int totalScorePart2 = 0;
 
   std::ifstream gameStrategyFile("input.txt");
   while (std::getline(gameStrategyFile, line)) {
     shape opponent = getShape(line.at(0));
     shape player = getShape(line.at(2));
+    result res = getResult(line.at(2));
+    shape play = getPlay(opponent, res);
 
     totalScore += shapeScore(player);
-    totalScore += resultScore(opponent, player);
+    totalScore += matchScore(opponent, player);
+
+    totalScorePart2 += shapeScore(play);
+    totalScorePart2 += resultScore(res);
   }
   gameStrategyFile.close();
   std::cout << totalScore << "\n";
+  std::cout << totalScorePart2 << "\n";
 }
